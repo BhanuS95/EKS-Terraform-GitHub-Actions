5beb879c631dcd0f493d04ea83040a8a0cb9957d
BhanuS95 <bs.shakya55@gmail.com>
2026-09-16 01:28:08 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "my-eks-cluster-bhanu"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lockfile   = true
     encrypt        = true
   }
 }
